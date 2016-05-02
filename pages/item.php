9b563389e342e012cb7c14ab2b49d9cdbfaabd4d
olegmarasin <olegmarasin@users.noreply.github.com>
2016-05-03 01:48:42 +0300

    Delete item.php

--- pages/item.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $number=$_GET['numb'];
-    $sql = "SELECT * FROM object o where o.number =".$number;
-    $query = mysqli_query($con, $sql);
-    $inf =  mysqli_fetch_assoc($query);
-    $title = $mass['title'];
-    $cost = $mass['cost'];
-    $date = $mass['offer-date'];
-?>
